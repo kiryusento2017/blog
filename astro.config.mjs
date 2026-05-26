e47e4e69fbcf36ca84109eed81adeddb2661a6cd
kiryusento <a1442846188@gmail.com>
2026-05-26 15:07:11 +0800

    fix: 设置正式域名 zmspblog.us.kg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zhongmo-shijian.example.com',
+  site: 'https://zmspblog.us.kg',
   server: { host: '0.0.0.0' },
   integrations: [react(), sitemap()]
 });
